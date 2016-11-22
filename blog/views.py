8338f73b2352eef4c8150dd853e05ad3f67fdad3
nyongee12 <nyongee12@gmail.com>
2016-11-22 22:45:50 +0900

    edit views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
